47c0d3c01c535b1a71c3bf4a55ab8b889856ace5
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-09-10 06:19:50 +0100

    updated main.js

--- javascript-warm_up/main_0.js
+++ javascript-warm_up/main_0.js
@@ -1,7 +1,6 @@
-const callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) {
-    theFunction();
-  }
-}
+const callMeMoby = require('./101-call_me_moby').callMeMoby;
+callMeMoby(5, function () {
+  console.log('Where I am!');
+});
 
   
