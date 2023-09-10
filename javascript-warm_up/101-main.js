56f2247c7cef10ceb209053194ce52a099b6a20f
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-09-10 15:09:27 +0100

    updated the main.js

--- javascript-warm_up/101-main.js
+++ javascript-warm_up/101-main.js
@@ -1,11 +1,4 @@
-function executeXTimes(x, theFunction) {
-  for (let i = 0; i < x; i++) {
-    theFunction();
-  }
-}
-
-function myFunction() {
-  console.log("Where I am!");
-}
-
-executeXTimes(5, myFunction);
+const callMeMoby = require('./101-call_me_moby').callMeMoby;
+callMeMoby(3, function () {
+  console.log('C is fun');
+});
