9a2fcd571ce09932486eab80704b69b9ff4d84e0
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-09-10 06:05:31 +0100

    updated the file

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,4 @@
-const callMeMoby = require('./101-call_me_moby').callMeMoby;
-callMeMoby(3, function () {
-  console.log('C is fun');
-});
-
-function executeXTimes(x, theFunction) {
+const callMeMoby = function (x, theFunction) {
     for (let i = 0; i < x; i++) {
       theFunction();
     }
