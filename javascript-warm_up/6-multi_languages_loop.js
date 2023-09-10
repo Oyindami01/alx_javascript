a9a488421afa472387df27e37e8014ad6d302b36
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-09-10 04:27:07 +0100

    reprinted the variables

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,8 @@ const my_Var = "C is fun"
 const myVar = "Python is cool"
 const _my_Var =   "JavaScript is amazing"
 
-for (let i = 0; i < lines.length; i++) {
-  console.log(lines[i]);
-}
+for (let i = 0; i < lines.length; i++)
+ 
+  console.log(my_Var);
+  console.log(MyVar);
+  console.log(_my_Var)
