082cde72ff2811622dea8acbab14376ec66d8e4d
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-09-10 04:28:36 +0100

    used only one console.log

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -3,7 +3,6 @@ const myVar = "Python is cool"
 const _my_Var =   "JavaScript is amazing"
 
 for (let i = 0; i < lines.length; i++)
- 
-  console.log(my_Var);
-  console.log(MyVar);
-  console.log(_my_Var)
+ {
+  console.log(lines[i]);
+}
