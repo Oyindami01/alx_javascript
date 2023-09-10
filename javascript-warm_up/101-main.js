d7ff4da01d05d0a51101b7aa7fb6601ba30efbd5
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-09-10 06:13:14 +0100

    updated main.js

--- javascript-warm_up/101-main.js
+++ javascript-warm_up/101-main.js
@@ -1,8 +1,3 @@
-const callMeMoby = function (x, theFunction) {
-    for (let i = 0; i < x; i++) {
-      theFunction();
-    }
-  }
 
   console.log(callMeMoby(5, function(){
     console.log('Where I am')
