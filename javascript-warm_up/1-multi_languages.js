ad4f531fb872a00d0cbb51da4cfb7cdf2387330c
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-09-10 04:18:26 +0100

    printed again

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,7 @@
-const myVar = "C is fun"
+const my_Var = "C is fun"
 const myVar = "Python is cool"
-const myVar = "JavaScript is amazing"
+const _my_Var = "JavaScript is amazing"
 console.log(myVar);
+console.log(_my_Var);
+console.log(my_Var);
 
