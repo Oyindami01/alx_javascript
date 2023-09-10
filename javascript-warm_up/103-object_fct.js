ea204e3998b01105b9932285590462728f83cb71
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-09-10 16:09:10 +0100

    updated 103-object

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,20 +1,21 @@
 #!/usr/bin/node
 
+const util = require('util');
 const myObject = {
-    type: 'object',
-    value: 12,
-    incr: function () {
-      this.value++;
-    }
-  };
-  
-  console.log(myObject);
-  
-  myObject.incr();
-  console.log(myObject);
-  
-  myObject.incr();
-  console.log(myObject);
-  
-  myObject.incr();
-  console.log(myObject);
+  type: 'object',
+  value: 12,
+  incr: function () {
+    this.value++;
+  }
+};
+
+console.log(util.inspect(myObject, { showHidden: false, depth: null }));
+
+myObject.incr();
+console.log(util.inspect(myObject, { showHidden: false, depth: null }));
+
+myObject.incr();
+console.log(util.inspect(myObject, { showHidden: false, depth: null }));
+
+myObject.incr();
+console.log(util.inspect(myObject, { showHidden: false, depth: null }));
