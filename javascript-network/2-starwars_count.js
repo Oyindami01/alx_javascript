3f411edc57040a79f84c8bda61381d676e8b79a2
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-10-13 07:20:20 +0100

    updated my code

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const request = require('request');
+const axios = require('axios');
 
 if (process.argv.length < 3) {
   console.error('Please provide the API URL as the first argument.');
@@ -9,36 +9,24 @@ if (process.argv.length < 3) {
 const apiUrl = process.argv[2];
 const characterId = 18;
 
-function getMovies (url) {
-  request.get(url, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error.message);
-      process.exit(1);
-    }
+async function getMovies(url) {
+  try {
+    const response = await axios.get(url);
+    const filmData = response.data;
 
-    if (response.statusCode !== 200) {
-      console.error(`Error: Request failed with status code ${response.statusCode}`);
-      process.exit(1);
-    }
+    const moviesWithWedgeAntilles = filmData.results.filter((movie) => {
+      return movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
+    });
 
-    
-    try {
-      const filmData = JSON.parse(body);
-      const moviesWithWedgeAntilles = filmData.results.filter(movie => {
-        return movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
-      });
-
-      if (filmData.next) {
-        getMovies(filmData.next);
-      } else {
-        console.log(moviesWithWedgeAntilles.length);
-      }
-    } catch (parseError) {
-      console.error('Error parsing API response:', parseError.message);
-      process.exit(1);
+    if (filmData.next) {
+      await getMovies(filmData.next);
+    } else {
+      console.log(moviesWithWedgeAntilles.length);
     }
-  });
+  } catch (error) {
+    console.error('Error:', error.message);
+    process.exit(1);
+  }
 }
 
 getMovies(apiUrl);
-
