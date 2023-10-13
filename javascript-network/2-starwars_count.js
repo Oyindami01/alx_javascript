aee72150e9a770e68e215c55d71a55a3b92913e2
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-10-13 07:06:30 +0100

    corrected the errors in my code

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -2,43 +2,29 @@
 const request = require('request');
 
 if (process.argv.length < 3) {
-  console.error('Please provide the API URL as the first argument.');
+  console.error('Please provide a movie ID as the first argument.');
   process.exit(1);
 }
 
-const apiUrl = process.argv[2];
-const characterId = 18;
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-let moviesWithWedgeAntilles = 0;
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error.message);
+    process.exit(1);
+  }
 
-function getMovies(url) {
-  request.get(url, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error.message);
-      process.exit(1);
-    }
+  if (response.statusCode !== 200) {
+    console.error(`Error: Request failed with status code ${response.statusCode}`);
+    process.exit(1);
+  }
 
-    if (response.statusCode !== 200) {
-      console.error(`Error: Request failed with status code ${response.statusCode}`);
-      process.exit(1);
-    }
-
-    try {
-      const filmData = JSON.parse(body);
-      moviesWithWedgeAntilles += filmData.results.filter(movie => {
-        return movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
-      }).length;
-
-      if (filmData.next) {
-        getMovies(filmData.next);
-      } else {
-        console.log(moviesWithWedgeAntilles);
-      }
-    } catch (parseError) {
-      console.error('Error parsing API response:', parseError.message);
-      process.exit(1);
-    }
-  });
-}
-
-getMovies(apiUrl);
+  try {
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
+  } catch (parseError) {
+    console.error('Error parsing API response:', parseError.message);
+    process.exit(1);
+  }
+});
